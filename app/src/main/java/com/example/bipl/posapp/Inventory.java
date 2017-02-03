d8e0c37575dbaca47ecec55ae7c8313385b2c2fd
fahad <mfahad88@outlook.com>
2017-02-03 11:40:37 +0500

    Begin

---
+++ app/src/main/java/com/example/bipl/posapp/Inventory.java
@@ -0,0 +1,40 @@
+package com.example.bipl.posapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+
+import com.example.bipl.posapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Inventory extends Fragment {
+TableLayout tableProduct;
+Button btn_scan,btn_add;
+EditText searchProduct;
+
+    public Inventory() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v=inflater.inflate(R.layout.fragment_inventory, container, false);
+        tableProduct=(TableLayout)v.findViewById(R.id.tableProduct);
+        btn_scan=(Button)v.findViewById(R.id.btn_scan);
+        btn_add=(Button)v.findViewById(R.id.btn_add);
+        searchProduct=(EditText)v.findViewById(R.id.search_product);
+        return v;
+    }
+
+}
